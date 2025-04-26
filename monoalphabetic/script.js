a6247feb6d3435696d808679790564adc66d6b3c
Evanazhr <evanazhr@gmail.com>
2025-04-26 14:16:16 +0700

    add : add monoalphabetic and palindrom

---
+++ monoalphabetic/script.js
@@ -0,0 +1,103 @@
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let key = alphabet;
+const plainText = document.querySelector(".plain-text");
+const inputKey = document.querySelector(".input-key");
+const KeyRow = document.querySelector(".key-row");
+const generateKeyBtn = document.querySelector(".generate-key-btn");
+const startSubtitutionBtn = document.querySelector(".start-subtitution-btn");
+const alphabetRow = document.querySelector(".alphabet-row");
+const output = document.querySelector(".output")
+
+
+//   create alphabet row
+for (let i = 0; i < alphabet.length; i++) {
+  const row = document.createElement("td"); // Buat elemen <td>
+  row.textContent = alphabet[i]; // Set textContent ke huruf yang sesuai
+  alphabetRow.appendChild(row); // Tambahkan <td> ke baris
+}
+
+function generateKey(value) {
+  const arrayKey = value.split(" ").join("").toUpperCase().split("");
+
+  // reduce duplicate data from arraykey
+  const uniqueKey = [];
+  arrayKey.forEach((value) => {
+    if (!uniqueKey.includes(value)) {
+      uniqueKey.push(value);
+    }
+  });
+
+  const uniqueKeyPlusAlphabet = (uniqueKey.join("") + alphabet).split("");
+
+  // reduce duplicate data from uniqueKeyPlusAlphabet
+  const fixKey = [].slice(0, 26);
+  uniqueKeyPlusAlphabet.forEach((value) => {
+    if (!fixKey.includes(value)) {
+      fixKey.push(value);
+    }
+  });
+
+  // create key row
+  KeyRow.innerHTML = "";
+  for (let i = 0; i < fixKey.length; i++) {
+    const row = document.createElement("td");
+    row.textContent = fixKey[i];
+    KeyRow.appendChild(row);
+  }
+
+  //   reassign global key
+  key = fixKey.join("");
+  console.log("Key = " + key);
+}
+
+generateKeyBtn.addEventListener("click", () => {
+  const inputKeyValue = inputKey.value;
+  generateKey(inputKeyValue);
+});
+
+inputKey.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const inputKeyValue = inputKey.value;
+    generateKey(inputKeyValue);
+  }
+});
+
+function startSubtitution(text, key) {
+  const arrayText = text.split("");
+  const arrayKey = key.split("");
+  const arrayAlphabet = alphabet.split("");
+
+  console.log(key);
+
+  const result = [];
+
+  for (let i = 0; i < arrayText.length; i++) {
+    const char = arrayText[i].toUpperCase();
+    const index = arrayAlphabet.indexOf(char);
+    console.log(char)
+    console.log(index)
+
+    if (index !== -1) {
+        result.push(arrayKey[index]); 
+        console.log("ada")
+    } else if (char == " ") {
+        console.log("spasi")
+        result.push(" "); 
+    } else {
+        console.log("tidak ada / simbol")
+        result.push(char)
+    }
+}
+
+console.log(result.join("")); 
+output.innerHTML = `<p>${result.join("")}</p>`
+}
+
+
+startSubtitutionBtn.addEventListener("click", () => {
+  const plainTextValue = plainText.value;
+  startSubtitution(plainTextValue, key);
+});
+
+
+// note : lama debugging gara gara lupa uppercase plain text, jadi indexnya gak ketemu temu di alphabet
---
+++ palindrom/script.js
@@ -0,0 +1,65 @@
+const input = document.querySelector("input");
+const button = document.querySelector(".button");
+const output = document.querySelector(".output");
+
+function palindromChecker(text) {
+  const textReverse = text.split("").reverse().join("");
+  console.log("text normal : " + text);
+  console.log("text reverse : " + textReverse);
+
+  if(text){
+      if (text === textReverse) {
+        output.innerHTML = `
+        <p class="output-text">Input : ${text} </p>
+        <p class="output-text">Reverse : ${textReverse} </p>
+        <p class="output-text">Palindrom 👍</p>
+        `;
+        console.log("Palindrom 👍");
+      } else {
+        output.innerHTML = `<p class="output-text">Input : ${text} </p>
+        <p class="output-text">Reverse : ${textReverse} </p>
+        <p class="output-text">Bukan Palindrom 👎</p>`;
+        console.log("Bukan Palindrom 👎");
+      }
+  }else{
+    output.innerHTML = `<p class="output-warning">Masukkan text dulu cuy...</p>`
+    
+  }
+}
+
+button.addEventListener("click", () => {
+  const inputValue = input.value;
+  palindromChecker(inputValue);
+});
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const inputValue = input.value;
+    palindromChecker(inputValue);
+  }
+});
+
+const info = document.querySelector(".palindrom-info")
+
+info.addEventListener("click", () => {
+    alert(`Palindrom adalah sebuah kata, frasa, angka, atau urutan karakter lainnya yang dapat dibaca sama dari depan maupun belakang. Dalam kata lain, jika Anda membalikkan urutan karakter tersebut, hasilnya tetap sama. Palindrom sering digunakan dalam berbagai konteks, termasuk dalam bahasa, matematika, dan pemrograman.
+
+        Contoh Palindrom:
+        Kata:
+        - "radar"
+        - "level"
+        - "madam"
+        - "civic"
+        
+        Frasa (dengan mengabaikan spasi dan tanda baca):
+        - "A man, a plan, a canal, Panama!"
+        - "Was it a car or a cat I saw?"
+        
+        Angka:
+        - 121
+        - 12321
+        - 45654
+        
+        Cara Memeriksa Palindrom:
+        Untuk memeriksa apakah sebuah string adalah palindrom, Anda dapat membandingkan karakter dari awal dan akhir string, kemudian bergerak ke arah tengah. Jika semua pasangan karakter yang dibandingkan sama, maka string tersebut adalah palindrom.`);
+})
